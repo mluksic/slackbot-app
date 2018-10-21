99429eca4dab95901fd95f10a5a90c279acc4a35
zpotokar <potokar.zan@gmail.com>
2018-10-21 07:55:10 +0200

    Create Shitty, Kudo, Graph, User page

---
+++ src/pages/graph-user-shitty/graph-user-shitty.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GraphUserShittyPage } from './graph-user-shitty';
+
+@NgModule({
+  declarations: [
+    GraphUserShittyPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(GraphUserShittyPage),
+  ],
+})
+export class GraphUserShittyPageModule {}
